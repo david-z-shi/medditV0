e1a79c7936cf47c8fe96ca83cccbc3d16f4a3026
ayushbansal <ayushbansal425@gmail.com>
2019-09-28 09:42:41 -0400

    Merge branch 'master' of /Users/Ayush/IdeaProjects/medditV0 with conflicts.

--- app/src/androidTest/java/com/example/android/medditv0/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/medditv0/ExampleInstrumentedTest.java
@@ -1,14 +1,12 @@
 package com.example.android.medditv0;
+import org.junit.Assert;
+import org.junit.Test;
 
 import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.

a3a3abfa8d3287b231e06fdd5493a1a8eaa54f96
David Shi <shizhaoshi@gmail.com>
2019-09-28 19:17:50 -0400

    david updating 4

---
+++ app/src/main/java/com/example/android/medditv0/main_menu.java
@@ -0,0 +1,56 @@
+package com.example.android.medditv0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class main_menu extends AppCompatActivity {
+    private Button UpdateProfile;
+    private Button SavedMatches;
+    private Button NewMatches;
+    private Button Settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        UpdateProfile = (Button) findViewById(R.id.UpdateProfile);
+        SavedMatches = (Button) findViewById(R.id.SavedMatches);
+        NewMatches = (Button) findViewById(R.id.NewMatches);
+        Settings = (Button) findViewById(R.id.Settings);
+
+        UpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_menu.this, update_user_preferences.class);
+                startActivity(intent);
+            }
+        });
+
+        SavedMatches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_menu.this, update_user_preferences.class);
+                startActivity(intent);
+            }
+        });
+
+        NewMatches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_menu.this, update_user_preferences.class);
+                startActivity(intent);
+            }
+        });
+
+        Settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_menu.this, update_user_preferences.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

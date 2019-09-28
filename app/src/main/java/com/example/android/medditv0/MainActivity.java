74021b494c49efb178e1335daaeffc0ca02e7760
David Shi <shizhaoshi@gmail.com>
2019-09-27 23:42:32 -0400

    copied text-to-speech code, crashes on debugger

--- app/src/main/java/com/example/android/medditv0/MainActivity.java
+++ app/src/main/java/com/example/android/medditv0/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.android.medditv0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainButton = (TextView) findViewById(R.id.mainButton);
+        Process();
+    }
+
+    private void Process() {
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                Intent intent = new Intent(MainActivity.this, text_to_speech.class);
+                startActivity(intent);
+            }
+        });
     }
 }

53f076f6fd899809b7a77d6a1ccd468c7f91aae8
Paulameena Shultes <pshultes@berkeley.edu>
2019-09-28 14:34:37 -0400

    UX progress

---
+++ app/src/main/java/com/example/android/medditv0/DatabaseUpload.java
@@ -0,0 +1,10 @@
+package com.example.android.medditv0;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.mongodb.clients.*;
+
+public class DatabaseUpload {
+    MongoClient uri = create(ConnectionString );
+}
